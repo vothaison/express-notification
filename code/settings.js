85d8e1bf2b6ea00ba427834dc850401cc2a49d3c
vothaison <son.thai.vo@steadfasttech.com.au>
2018-10-14 13:44:13 +0700

    DEBUG

--- code/settings.js
+++ code/settings.js
@@ -15,7 +15,7 @@ module.exports = {
                 fitUrl: 'http://fit.sgu.edu.vn/web2017/',
                 saveUrl: 'https://paper-by-vts.000webhostapp.com/Bridge/save_fit_json',
                 getUrl: 'https://paper-by-vts.000webhostapp.com/Bridge/get_latest_fit_json',
-                getNotificationSubscriptionsUrl: 'https://paper-by-vts.000webhostapp.com/DataBridge/list_subscription',
+                getNotificationSubscriptionsUrl: 'https://paper-by-vts.000webhostapp.com/Bridge/list_subscription',
             };
         }
     }
